2930848c6584761806b1815cec1253b1d163d838
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-06 14:25:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 
 //your JS code here. If required.
 let myBtn=document.getElementById("enterBtn");
-let mouse=document.getElementBYId("status");
-
 myBtn.addEventListener("click",(e)=>{
-	mouse.innerText="Entered Metaverse";
-	mouse.outerHTML="<h1 id='status'>"+statusParagraph.innerHTML+"</h1>";
+	console.log(clicked);
 });
