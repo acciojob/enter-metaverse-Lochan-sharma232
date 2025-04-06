7651879889e26ba3cd8e884dc2e0b760dc10e9ec
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-06 14:32:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 //your JS code here. If required.
 let myBtn=document.getElementById("enterBtn");
+let status=document.getElementById("status");
+let enteredMetaverse=document.getElementById("enteredMetaverse");
 myBtn.addEventListener("click",(e)=>{
 	console.log(clicked);
 });
