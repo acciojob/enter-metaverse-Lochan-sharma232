0a6bad0c808c900744e4f1ac1da050b8ca7cd388
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-06 18:43:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-
-//your JS code here. If required.
 let myBtn=document.getElementById("enterBtn");
 let parent=document.getElementById("parent");
 
-function changetext()
-	parent.innerHTML="<H1 id="status">Entered Metaverse</H1>";
+function changetext(){
+	parent.innerHTML="";
+	parent.innerHTML=`<H1 id="status">Entered Metaverse</H1>`;
 }
 myBtn.addEventListener("click",(e)=>{
-changetext();
+	changetext();
 });
