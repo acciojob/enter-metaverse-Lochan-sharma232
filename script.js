c773be04d543dab5d6fb76e625f23012af4f52d1
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-06 13:42:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let 
+let parentDiv=documnent.getElementById("parent");
+console.log("parent",parentDiv);
 //your JS code here. If required.
 let myBtn=document.getElementById("enterBtn");
 myBtn.addEventListener("click",(e)=>{
