878680add869f81f6be2866bb54aaaa38be9614f
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-06 14:37:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,11 @@
 let myBtn=document.getElementById("enterBtn");
 let status=document.getElementById("status");
 let enteredMetaverse=document.getElementById("enteredMetaverse");
+function changetext(){
+	status.innerText="Enter the Metaverse";
+	enteredMetaverse="Entered Metaverse";
+}
 myBtn.addEventListener("click",(e)=>{
 	console.log(clicked);
+	changetext();
 });
